38613605418087d50ab9353e8e3af13411a95e2f
余胜 <wx_a2f53f9499804ae094c7d8ca7817d653@git.code.tencent.com>
2019-12-18 10:40:43 +0800

    1.0.0版本，正式支持Android，ios双端支付

--- android/src/main/java/com/zaixiaoqu/alipay/RnAlipayModulePackage.java
+++ android/src/main/java/com/zaixiaoqu/alipay/RnAlipayModulePackage.java
@@ -9,11 +9,15 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
+import com.zaixiaoqu.alipay.component.AlipayModule;
 
 public class RnAlipayModulePackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Arrays.<NativeModule>asList(new RnAlipayModuleModule(reactContext));
+        return Arrays.<NativeModule>asList(
+                new RnAlipayModuleModule(reactContext),
+                new AlipayModule(reactContext)
+        );
     }
 
     @Override
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { NativeModules } from 'react-native';
+import { Alipay } from './lib';
 
 const { RnAlipayModule } = NativeModules;
 
 export default RnAlipayModule;
+export {
+    Alipay
+};
